42c7e56e7d63965b41c5e65efd2bd5d89dacefd9
nh40 <imbovoebozestvo@gmail.com>
2025-01-18 10:49:41 +0700

    stream list home

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,15 @@ import createNextIntlPlugin from 'next-intl/plugin'
 const withNextIntl = createNextIntlPlugin('./src/libs/i18n/request.ts')
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'b154eedd-5cd2-468f-9ac7-c6107e934ab8.selstorage.ru'
+      }
+    ]
+  }
 }
 
 export default withNextIntl(nextConfig)
